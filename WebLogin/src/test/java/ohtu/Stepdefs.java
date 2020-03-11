3bb0b14f50f06af317cef35e0885e83d9ecbb750
Axamabrotha <aku.hirvonen@gmail.com>
2020-03-11 21:25:31 +0200

    viikko 3 tehtävä 10, korjaus tehtävään 6

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -67,6 +67,58 @@ public class Stepdefs {
         assertTrue(driver.getPageSource().contains(pageContent));
     }
     
+    @Given("command new user is selected")
+    public void newIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();   
+    } 
+       
+    @When("a valid username {string} and password {string} and matching password confirmation {string} are entered")
+    public void creationSuccessful(String username, String password, String passwordConfirmation) throws Throwable {
+        createAccountWith(username, password, passwordConfirmation);
+    } 
+    
+    @Then("a new user is created")
+    public void creationSuccessfulLogin() {
+        pageHasContent("Welcome to Ohtu Application!");
+//        String pageContent = "Ohtu Application main page";
+//        assertTrue(driver.getPageSource().contains(pageContent));
+    }
+    
+    @When("too short username {string} and valid password {string} with matching password confirmation {string} are entered")
+    public void tooShortUsername(String username, String password, String passwordConfirmation) throws Throwable {
+        createAccountWith(username, password, passwordConfirmation);
+    } 
+    
+//    @Then("user is not created and error {string} is reported")
+//    public void tooShortUserError() {
+//        pageHasContent("username should have at least 3 characters");
+//    }  
+    
+    @When("valid username {string} and too short password {string} with matching password confirmation {string} are entered")
+    public void tooShortPassword(String username, String password, String passwordConfirmation) throws Throwable {
+        createAccountWith(username, password, passwordConfirmation);
+    } 
+    
+//    @Then("user is not created and error {string} is reported")
+//    public void tooShortPwdError() {
+//        pageHasContent("password should have at least 8 characters");
+//    } 
+    
+    @When("valid username {string} and valid password {string} are entered but password and password confirmation {string} do not match")
+    public void pwdConfirmationNoMatch(String username, String password, String passwordConfirmation) throws Throwable {
+        createAccountWith(username, password, passwordConfirmation);
+    } 
+    
+    @Then("user is not created and error {string} is reported")
+    public void invalidCredentialsError(String pageContent) throws Throwable {
+        assertTrue(driver.getPageSource().contains(pageContent));
+//        pageHasContent("username should have at least 3 characters");
+//        pageHasContent("password should have at least 8 characters");
+//        pageHasContent("password and password confirmation do not match");
+    }
+    
     @After
     public void tearDown(){
         driver.quit();
@@ -87,4 +139,16 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+    
+    private void createAccountWith(String username, String password, String passwordConfirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConfirmation);
+        element = driver.findElement(By.name("signup"));
+        element.submit();  
+    }
 }
